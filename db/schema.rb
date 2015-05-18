f9dedbfac23a1eb1abb6f727e49f29e9b3645296
smith <smith@futueworkz.com>
2015-05-18 14:17:57 +0700

    Change products table price to 10 precision

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150518065826) do
+ActiveRecord::Schema.define(version: 20150518071446) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20150518065826) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.decimal  "price",       precision: 6, scale: 2
+    t.decimal  "price",       precision: 10, scale: 2
     t.boolean  "published"
     t.integer  "category_id"
   end
