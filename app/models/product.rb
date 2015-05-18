92a0c33f9b59bfd15af2c7f19977170e94265b36
smith <smith@futueworkz.com>
2015-05-18 17:18:25 +0700

    Add product model with validation

---
+++ app/models/product.rb
@@ -0,0 +1,12 @@
+class Product < ActiveRecord::Base
+	validates :title, :description, presence: true
+	validates :price, presence: true, numericality: {greater_than: 0}
+	validate :title_is_shorter_than_description
+
+	def title_is_shorter_than_description
+		return if title.blank? or description.blank?
+		if description.length < title.length
+			errors.add(:description, 'can\'t be shorter than title')
+		end
+	end
+end 
