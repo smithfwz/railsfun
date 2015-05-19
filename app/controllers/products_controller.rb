42ca6202ab9ebf9253fb0d274f8c2abd4b8cf368
smith <smith@futueworkz.com>
2015-05-19 16:34:45 +0700

    Add products controller and views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,9 @@ class ProductsController < ApplicationController
 	def index
 		@products = Product.includes(:category).published
 	end
+
+	def show
+		#@product = Product.find(params[:id])
+		@product = Product.find(params.require(:id))
+	end
 end
