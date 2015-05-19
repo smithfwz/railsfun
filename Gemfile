a76be3715a8780cd8c97e59314ba74f1260046a5
smith <smith@futueworkz.com>
2015-05-19 17:48:02 +0700

    Create form new product

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'enumerize'
 
 gem 'countries'
 
+gem 'formtastic'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,11 @@ class ProductsController < ApplicationController
 	end
 
 	def show
-		#@product = Product.find(params[:id])
-		@product = Product.find(params.require(:id))
+		#@product = Product.find(params[:id])	//type 1
+		@product = Product.find(params.require(:id)) #type 2
+	end
+
+	def new
+		@product = Product.new
 	end
 end
