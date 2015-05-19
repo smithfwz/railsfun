4e03d0e5ebeab70450befd71f68a8fd1b378c5c4
smith <smith@futueworkz.com>
2015-05-19 10:58:18 +0700

    Update product model: using before_save and before_validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
+	before_validation :strip_html_from_description
+	before_save :change_title_to_downcase
+
 	validates :title, :description, presence: true
 	validates :price, presence: true, numericality: {greater_than: 0}
 	validate :title_is_shorter_than_description
@@ -7,10 +10,20 @@ class Product < ActiveRecord::Base
 	scope :priced_more_than,  	 ->(price){ where('price > ?', price) }
 	scope :description_includes, ->(description){ where('description ILIKE ?', "%#{description}%") }
 
+	
+
 	def title_is_shorter_than_description
 		return if title.blank? or description.blank?
 		if description.length < title.length
 			errors.add(:description, 'can\'t be shorter than title')
 		end
+	end
+
+	def strip_html_from_description
+		self.description = ActionView::Base.full_sanitizer.sanitize(self.description)
+	end
+
+	def change_title_to_downcase
+		self.title = self.title.downcase
 	end
 end
