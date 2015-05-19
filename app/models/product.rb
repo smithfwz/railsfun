be94528a57649670ab8a38fad985247147fe638c
smith <smith@futueworkz.com>
2015-05-19 10:30:07 +0700

    Update product model: using scopes

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,10 @@ class Product < ActiveRecord::Base
 	validates :price, presence: true, numericality: {greater_than: 0}
 	validate :title_is_shorter_than_description
 
+	scope :published, 					 ->{ where(published: true) }
+	scope :priced_more_than,  	 ->(price){ where('price > ?', price) }
+	scope :description_includes, ->(description){ where('description ILIKE ?', "%#{description}%") }
+
 	def title_is_shorter_than_description
 		return if title.blank? or description.blank?
 		if description.length < title.length
